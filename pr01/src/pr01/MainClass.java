b8cfde48f818f15023029aec86d1f72d60a83345
becca4011 <becca4011@naver.com>
2020-03-05 18:02:51 +0900

    java

---
+++ pr01/src/pr01/MainClass.java
@@ -0,0 +1,11 @@
+package pr01;
+
+public class MainClass 
+{
+	//main을 입력하고 ctrl + space를 누른 후 main(String[] args)를 선택하면 자동완성
+	public static void main(String[] args) 
+	{
+		//sysout을 입력하고 ctrl + space를 누르면 자동 완성
+		System.out.println("Hello Java World!");
+	}
+}
